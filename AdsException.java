2c42f794c50780526cc79134043f2eacde016d6b
Alexandr <melifaro09@gmail.com>
2017-01-31 15:38:39 +0100

    Update AdsException.java

--- AdsException.java
+++ AdsException.java
@@ -1,17 +1,30 @@
 /**
+ * Copyright 2016-2017 Alexandr Mitiaev
  * 
+ * This file is part of Beckhoff ADS library.
+ * 
+ * Beckhoff ADS library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beckhoff ADS library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Beckhoff ADS library. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.whz.research.tempcontrol.sps;
 
 /**
- * Exception class for ADS-functions
- * 
- * @author Alexandr Mitiaev
+ * Exception class for ADS functions
  */
 public class AdsException extends Exception {
 	
 	/**
-	 * Integer error code
+	 * Integer error code value
 	 */
 	private Integer errorCode;
 	
@@ -21,7 +34,7 @@ public class AdsException extends Exception {
 	private static final long serialVersionUID = -7061270224430606676L;
 	
 	/**
-	 * Create new {@link AdsException} object
+	 * Create new {@link AdsException} object with error code = 0
 	 */
 	public AdsException() {
 		this.errorCode = 0;
@@ -30,39 +43,37 @@ public class AdsException extends Exception {
 	/**
 	 * Create new {@link AdsException} object with error code = 0
 	 * 
-	 * @param message Error message
+	 * @param message Message with a description of the errror
 	 */
 	public AdsException(String message) {
 		super(message);
-		
 		this.errorCode = 0;
 	}
 	
 	/**
-	 * Create new {@link AdsException} object
+	 * Create new {@link AdsException} object with a given error code
 	 * 
 	 * @param errorCode Error code
-	 * @param message Error message
+	 * @param message Message with a description of the error
 	 */
 	public AdsException(Integer errorCode, String message) {
 		super(message);
-		
 		this.errorCode = errorCode;
 	}
 	
 	/**
 	 * Returns code of the error
 	 * 
-	 * @return error code
+	 * @return Error code
 	 */
 	public Integer getErrorCode() {
 		return this.errorCode;
 	}
 	
 	/**
-	 * Set new error code
+	 * Set new error code value
 	 * 
-	 * @param errorCode new value for {@link #errorCode}
+	 * @param errorCode New value for error code
 	 */
 	public void setErrorCode(Integer errorCode) {
 		this.errorCode = errorCode;
